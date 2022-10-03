47a1a02242413772a24dc53ee7a9ce847415846d
Subaraki <robbeenaxel@hotmail.com>
2022-10-03 21:52:49 +0200

    update to 1.19.2

--- src/main/java/com/jackdaw/events/KeyRegistry.java
+++ src/main/java/com/jackdaw/events/KeyRegistry.java
@@ -3,10 +3,9 @@ package com.jackdaw.events;
 import com.jackdaw.mod.SnowMod;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(modid = SnowMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -15,14 +14,9 @@ public class KeyRegistry {
     public static KeyMapping peekey;
 
     @SubscribeEvent
-    public static void launchClientEvent(FMLClientSetupEvent event) {
-        new KeyRegistry().registerKey();
-    }
-
-    public void registerKey() {
-
+    public static void launchClientEvent(RegisterKeyMappingsEvent event) {
         peekey = new KeyMapping("Pee key", GLFW.GLFW_KEY_P, "Pee");
-        ClientRegistry.registerKeyBinding(peekey);
+        event.register(peekey);
     }
 
 }
